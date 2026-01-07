16f685216122e7a4ffc01c6accc712f4bcaaeb17
TalhaDevDaily <talha@boishakhikunja.onmicrosoft.com>
2026-01-07 15:18:24 +0600

    All Wipe

--- dbConfig/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const dbConfig = function () {
-  return mongoose.connect(process.env.MONGODB_URI).then(function () {
-    console.log("DataBase is Connected! 📔");
-  });
-};
-
-module.exports = dbConfig;
--- routes/shortnerRoute.js
+++
@@ -1,7 +0,0 @@
-const _ = require("express").Router();
-
-_.get("/shortner", function (req, res) {
-  res.send("URL shortner was supposed to be here");
-});
-
-module.exports = _;
